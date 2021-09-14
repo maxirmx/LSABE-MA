54e43ca1bb034e4dd6ac7c1eb0b1b5de6a679946
Maxim Samsonov <m.samsonov@computer.org>
2021-09-14 09:41:53 +0300

    B17

--- scripts/ensure-pbc.sh
+++ scripts/ensure-pbc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz && \
 tar -xvf pbc-0.5.14.tar.gz && \
-cd pbc-0.5.14 && ./configure --prefix=$HOME/charm-crypto && make install 
+cd pbc-0.5.14 && ./configure && make install 
